501770cf8bffca87aaa74ea045b337332009dd23
gabrielpdasilva <gahsilva1177@gmail.com>
2022-09-29 21:04:50 -0300

    Primeiro commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+let count = 0;
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+    count++;
+    CURRENT_NUMBER.innerHTML = count;
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+    count--;
+    CURRENT_NUMBER.innerHTML = count;
+}
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+
+function test() {
+	kdowkdpo;
+}
